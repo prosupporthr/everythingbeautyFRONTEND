90eadeb1d58f5a636cfba6788690a5ab1a654890
egileekada <egileoniso.ekada@gmail.com>
2026-02-25 14:25:19 +0100

    fix: enhance business header and business model for improved functionality
    - Added routing logic in BusinessHeader to handle the case when no tab is selected, directing users to the edit business page.
    - Updated the button label in BusinessHeader to reflect the current context more accurately.
    - Made the pictures property in the IBusiness interface optional for better flexibility in business data handling.
    - Added licenseNumber to the IBusinessDetails interface to ensure comprehensive business information is captured.

--- src/hooks/useBusiness.ts
+++ src/hooks/useBusiness.ts
@@ -1,29 +1,35 @@
-"use client"
-import { handleError } from "@/helper/services/errorHandler"
-import httpService from "@/helper/services/httpService"
-import { addToast } from "@heroui/toast"
-import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { useFormik } from "formik"
-import { useParams, useRouter } from "next/navigation"
-import { useUploadMutation } from "./useUpload"
-import { URLS } from "@/helper/services/urls"
-import { useState } from "react" 
-import { businessSchema, productSchema, serviceSchema } from "@/helper/services/validation"
-import { IBookingmark, IBusiness, IProduct, IServices } from "@/helper/model/business"
-import { useUserStore } from "./user"
+"use client";
+import { handleError } from "@/helper/services/errorHandler";
+import httpService from "@/helper/services/httpService";
+import { addToast } from "@heroui/toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useFormik } from "formik";
+import { useParams, useRouter } from "next/navigation";
+import { useUploadMutation } from "./useUpload";
+import { URLS } from "@/helper/services/urls";
+import { useState } from "react";
+import {
+    businessSchema,
+    productSchema,
+    serviceSchema,
+} from "@/helper/services/validation";
+import {
+    IBookingmark,
+    IBusiness,
+    IProduct,
+    IServices,
+} from "@/helper/model/business";
+import { useUserStore } from "./user";
 
 interface IProps {
-    services?: boolean,
-    product?: boolean,
-    business?: boolean
+    services?: boolean;
+    product?: boolean;
+    business?: boolean;
 }
 
-const useBusiness = (
-    { services, product, business }: IProps
-) => {
-
-    const router = useRouter()
-    const userId = localStorage.getItem("userid") as string
+const useBusiness = ({ services, product, business }: IProps) => {
+    const router = useRouter();
+    const userId = localStorage.getItem("userid") as string;
     const [imageFile, setImageFile] = useState<File | string | null>("");
 
     const { refetch } = useUserStore();
@@ -32,38 +38,53 @@ const useBusiness = (
     const id = param.id as string;
     const slug = param.slug as string;
 
-    const queryClient = useQueryClient()
+    const queryClient = useQueryClient();
 
     /** 🔹 Business */
     const businessMutation = useMutation({
-        mutationFn: (data: IBusiness) =>
-            httpService.post(URLS.BUSINESS, data),
+        mutationFn: (data: IBusiness) => httpService.post(URLS.BUSINESS, data),
         onError: handleError,
         onSuccess: (res) => {
             addToast({
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            })
-            refetch()
-            router.push(`/`)
+            });
+            queryClient.invalidateQueries({ queryKey: ["business"] });
+            refetch();
+            router.push(`/`);
         },
-    })
+    });
+
+    /** 🔹 Business */
+    const businessEditMutation = useMutation({
+        mutationFn: (data: IBusiness) =>
+            httpService.patch(URLS.BUSINESSBYID(id), data),
+        onError: handleError,
+        onSuccess: (res) => {
+            addToast({
+                title: "Success",
+                description: res?.data?.message,
+                color: "success",
+            });
+            queryClient.invalidateQueries({ queryKey: ["business"] });
+            router.push(`/business/${id}/dashboard`);
+        },
+    });
 
     /** 🔹 Service */
     const servicesMutation = useMutation({
-        mutationFn: (data: IServices) =>
-            httpService.post(URLS.SERVICE, data),
+        mutationFn: (data: IServices) => httpService.post(URLS.SERVICE, data),
         onError: handleError,
         onSuccess: (res) => {
             addToast({
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            })
-            router.push(`/business/${id}/dashboard?tab=services`)
+            });
+            router.push(`/business/${id}/dashboard?tab=services`);
         },
-    })
+    });
 
     /** 🔹 Service */
     const servicesEditMutation = useMutation({
@@ -75,12 +96,12 @@ const useBusiness = (
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            })
+            });
 
-            queryClient.invalidateQueries({ queryKey: ["service"] })
-            router.push(`/business/${id}/dashboard?tab=services`)
+            queryClient.invalidateQueries({ queryKey: ["service"] });
+            router.push(`/business/${id}/dashboard?tab=services`);
         },
-    })
+    });
 
     /** 🔹 Service */
     const servicesDeleteMutation = useMutation({
@@ -92,25 +113,24 @@ const useBusiness = (
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            })
-            queryClient.invalidateQueries({ queryKey: ["service"] })
+            });
+            queryClient.invalidateQueries({ queryKey: ["service"] });
         },
-    })
+    });
 
     /** 🔹 Product */
     const productMutation = useMutation({
-        mutationFn: (data: IProduct) =>
-            httpService.post(URLS.PRODUCT, data),
+        mutationFn: (data: IProduct) => httpService.post(URLS.PRODUCT, data),
         onError: handleError,
         onSuccess: (res) => {
             addToast({
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            })
-            router.push(`/business/${id}/dashboard?tab=store`)
+            });
+            router.push(`/business/${id}/dashboard?tab=store`);
         },
-    })
+    });
 
     /** 🔹 Product */
     const bookmarkMutation = useMutation({
@@ -122,14 +142,13 @@ const useBusiness = (
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            }) 
-            queryClient.invalidateQueries({ queryKey: ["service"] })
-            queryClient.invalidateQueries({ queryKey: ["business"] })
-            queryClient.invalidateQueries({ queryKey: ["bookmarks"] })
-            queryClient.invalidateQueries({ queryKey: ["product"] })
+            });
+            queryClient.invalidateQueries({ queryKey: ["service"] });
+            queryClient.invalidateQueries({ queryKey: ["business"] });
+            queryClient.invalidateQueries({ queryKey: ["bookmarks"] });
+            queryClient.invalidateQueries({ queryKey: ["product"] });
         },
-    })
-
+    });
 
     /** 🔹 Product */
     const bookmarkdeleteMutation = useMutation({
@@ -141,10 +160,10 @@ const useBusiness = (
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            })  
-            queryClient.invalidateQueries({ queryKey: ["bookmarks"] }) 
+            });
+            queryClient.invalidateQueries({ queryKey: ["bookmarks"] });
         },
-    })
+    });
 
     /** 🔹 Product */
     const productEditMutation = useMutation({
@@ -156,12 +175,12 @@ const useBusiness = (
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            })
+            });
 
-            queryClient.invalidateQueries({ queryKey: ["product"] })
-            router.push(`/business/${id}/dashboard?tab=store`)
+            queryClient.invalidateQueries({ queryKey: ["product"] });
+            router.push(`/business/${id}/dashboard?tab=store`);
         },
-    })
+    });
 
     /** 🔹 Product */
     const productDeleteMutation = useMutation({
@@ -173,124 +192,144 @@ const useBusiness = (
                 title: "Success",
                 description: res?.data?.message,
                 color: "success",
-            }) 
-            queryClient.invalidateQueries({ queryKey: ["product"] })
+            });
+            queryClient.invalidateQueries({ queryKey: ["product"] });
         },
-    })
+    });
 
     const uploadMutation = useUploadMutation((res) => {
-
-        const payload = { ...formik.values, pictures: [res + ""] }
-        const payloadservices = { ...formikService.values, pictures: [res + ""] }
-        const payloadproduct = { ...formikProduct.values, pictures: [res + ""] }
+        const payload = { ...formik.values, pictures: [res + ""] };
+        const payloadservices = {
+            ...formikService.values,
+            pictures: [res + ""],
+        };
+        const payloadproduct = {
+            ...formikProduct.values,
+            pictures: [res + ""],
+        };
 
         if (business) {
-            businessMutation.mutate(payload)
+            if (id) {
+                businessEditMutation.mutate(payload);
+            } else {
+                businessMutation.mutate(payload);
+            }
         } else if (services) {
             if (slug) {
-                servicesEditMutation.mutate(payloadservices)
+                servicesEditMutation.mutate(payloadservices);
             } else {
-                servicesMutation.mutate(payloadservices)
+                servicesMutation.mutate(payloadservices);
             }
         } else if (product) {
             if (slug) {
-                productEditMutation.mutate(payloadproduct)
+                productEditMutation.mutate(payloadproduct);
             } else {
-                productMutation.mutate(payloadproduct)
+                productMutation.mutate(payloadproduct);
             }
         }
-    })
+    });
 
     /** 🔹 Formik Instances */
-    const formik = useFormik({
+    const formik = useFormik<IBusiness>({
         initialValues: {
-            "userId": userId,
-            "name": "",
-            "location": "",
-            "long": "",
-            "lat": "",
-            "days": [],
-            "openingTime": "",
-            "closingTime": "",
-            "chargeTiming": "",
-            "pictures": [],
-            licenseNumber: ""
+            userId: userId,
+            name: "",
+            location: "",
+            long: "",
+            lat: "",
+            days: [],
+            openingTime: "",
+            closingTime: "",
+            chargeTiming: "",
+            pictures: [],
+            licenseNumber: "",
         },
         validationSchema: businessSchema,
-        onSubmit: () => {
-            if (imageFile) {
-                const formdata = new FormData()
-                formdata.append("file", imageFile)
-                uploadMutation.mutate(formdata)
+        onSubmit: (data) => {
+            if (id && !imageFile) {
+                businessEditMutation.mutate(data);
+            } else if (imageFile) {
+                const formdata = new FormData();
+                formdata.append("file", imageFile);
+                uploadMutation.mutate(formdata);
             } else {
                 addToast({
                     title: "Error",
                     description: "At least one picture is required",
                     color: "danger",
-                })
-            }
+                });
+            } 
         },
-    })
-
+    });
 
     /** 🔹 Formik Instances */
     const formikService = useFormik<IServices>({
         initialValues: {
-            "businessId": slug ? "" : id,
-            "name": "",
-            "description": "",
-            "hourlyRate": "",
-            "allowReview": false, 
+            businessId: slug ? "" : id,
+            name: "",
+            description: "",
+            hourlyRate: "",
+            allowReview: false,
             initialDepositPercentage: "",
-            acceptsInitialDeposit: true
+            acceptsInitialDeposit: true,
         },
         validationSchema: serviceSchema,
         onSubmit: (data) => {
             if (slug && !imageFile) {
-                servicesEditMutation.mutate(data)
+                servicesEditMutation.mutate(data);
             } else if (imageFile) {
-                const formdata = new FormData()
-                formdata.append("file", imageFile)
-                uploadMutation.mutate(formdata)
+                const formdata = new FormData();
+                formdata.append("file", imageFile);
+                uploadMutation.mutate(formdata);
             } else {
                 addToast({
                     title: "Error",
                     description: "At least one picture is required",
                     color: "danger",
-                })
+                });
             }
         },
-    })
+    });
 
     /** 🔹 Formik Instances */
     const formikProduct = useFormik<IProduct>({
         initialValues: {
-            "businessId": slug ? "" : id,
-            "name": "",
-            "description": "",
-            "price": "",
-            "allowReview": false,
-            quantity: ""
+            businessId: slug ? "" : id,
+            name: "",
+            description: "",
+            price: "",
+            allowReview: false,
+            quantity: "",
         },
         validationSchema: productSchema,
         onSubmit: (data) => {
             if (slug && !imageFile) {
-                productEditMutation.mutate(data)
+                productEditMutation.mutate(data);
             } else if (imageFile) {
-                const formdata = new FormData()
-                formdata.append("file", imageFile)
-                uploadMutation.mutate(formdata)
+                const formdata = new FormData();
+                formdata.append("file", imageFile);
+                uploadMutation.mutate(formdata);
             } else {
                 addToast({
                     title: "Error",
                     description: "At least one picture is required",
                     color: "danger",
-                })
+                });
             }
         },
-    })
+    });
 
-    const isLoading = uploadMutation.isPending || businessMutation.isPending || servicesMutation.isPending || servicesEditMutation.isPending || servicesDeleteMutation.isPending || productMutation.isPending || productEditMutation.isPending || productDeleteMutation.isPending || bookmarkMutation.isPending || bookmarkdeleteMutation.isPending
+    const isLoading =
+        uploadMutation.isPending ||
+        businessMutation.isPending ||
+        servicesMutation.isPending ||
+        servicesEditMutation.isPending ||
+        servicesDeleteMutation.isPending ||
+        productMutation.isPending ||
+        productEditMutation.isPending ||
+        productDeleteMutation.isPending ||
+        bookmarkMutation.isPending ||
+        bookmarkdeleteMutation.isPending;
 
     return {
         formik,
@@ -303,8 +342,8 @@ const useBusiness = (
         bookmarkdeleteMutation,
         userId,
         setImageFile,
-        imageFile
-    }
-}
+        imageFile,
+    };
+};
 
-export default useBusiness
+export default useBusiness;
