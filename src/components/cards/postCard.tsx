7dccb7ef9584e6703df6b2c04b27a1c58ec6062b
egileekada <egileoniso.ekada@gmail.com>
2026-06-16 11:53:54 +0100

    fix: improve null safety and URL structure in PostCard and URL services
    - Updated PostCard component to use optional chaining for safer access to business properties, enhancing robustness against undefined values.
    - Modified URL service to correct the endpoint for comment replies, ensuring accurate routing for comment-related actions.

--- src/components/cards/postCard.tsx
+++ src/components/cards/postCard.tsx
@@ -74,18 +74,18 @@ export default function PostCard({
         >
             <div className=" w-full flex items-center justify-between pt-4 px-4 ">
                 <div className=" flex items-center gap-2 ">
-                    {item.business.pictures && (
+                    {item?.business?.pictures && (
                         <div className=" w-10 h-10 rounded-full bg-gray-400 ">
                             <Avatar
                                 size={"md"}
-                                src={item.business.pictures[0]}
+                                src={item?.business?.pictures[0]}
                                 name={item?.business.name}
                             />
                         </div>
                     )}
                     <div className=" flex flex-col ">
                         <p className=" text-sm font-bold capitalize ">
-                            {item?.business.name}
+                            {item?.business?.name}
                         </p>
                         <p className=" text-xs text-secondary ">
                             {textLimit(item?.business?.location, 20)}
